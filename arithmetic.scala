febaeb61998e32b6c9c7b2b19eaab907d4342e76
Edward Huang <edwardgunawan880@yahoo.com>
2020-03-02 22:48:32 -0500

    problem 36

--- arithmetic.scala
+++ arithmetic.scala
@@ -72,6 +72,21 @@ package arithmetic {
             primeFactorsR(start,primes)
         }
 
+        /*
+            P36
+            Determine the prime factors of a given positive integer (2).
+            Construct a list containing the prime factors and their multiplicity.
+            scala> 315.primeFactorMultiplicity
+            res0: List[(Int, Int)] = List((3,2), (5,1), (7,1))
+            Alternately, use a Map for the result.
+
+            scala> 315.primeFactorMultiplicity
+            res0: Map[Int,Int] = Map(3 -> 2, 5 -> 1, 7 -> 1)
+        */
+        def primeFactorMultiplicity: List[(Int,Int)] = {
+            start.primeFactors.groupBy(identity).mapValues(_.size).toList
+        }
+
 
         
 
