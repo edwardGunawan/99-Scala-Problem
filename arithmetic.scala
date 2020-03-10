c32411467692a6ade379728bbc83adf831cc4e69
Edward Huang <edwardgunawan880@yahoo.com>
2020-03-10 00:11:14 -0400

    problem 38

--- arithmetic.scala
+++ arithmetic.scala
@@ -1,4 +1,7 @@
 package arithmetic {
+
+    import java.time.Instant
+    import scala.concurrent.Future
     class S99Int(val start:Int) {
         import S99Int._
 
@@ -87,6 +90,45 @@ package arithmetic {
             start.primeFactors.groupBy(identity).mapValues(_.size).toList
         }
 
+        /*
+            P37
+            Calculate Euler's totient function phi(m) (improved).
+            See problem P34 for the definition of Euler's totient function. 
+            If the list of the prime factors of a number m is known in the form of 
+            problem P36 then the function phi(m>) can be efficiently calculated as follows: 
+            Let [[p1, m1], [p2, m2], [p3, m3], ...] be the list of prime factors (and their multiplicities) of a given number m. 
+            Then phi(m) can be calculated with the following formula:
+            phi(m) = (p1-1)*p1(m1-1) * (p2-1)*p2(m2-1) * (p3-1)*p3(m3-1) * ...
+
+            Note that ab stands for the bth power of a.
+        */
+        def totientImprov: Int = start.primeFactorMultiplicity.foldLeft(1){(acc,tup) =>
+            val (p, m) = tup
+            acc * ((p-1) * Math.pow((p),m-1)).toInt
+        }
+
+        /*
+         P38
+         (*) Compare the two methods of calculating Euler's totient function.
+            Use the solutions of problems P34 and P37 to compare the algorithms. 
+            Try to calculate phi(10090) as an example.
+        */
+        def phi:Unit = {
+            implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+            for {
+                regularTotient <- Future{
+                    val initial = Instant.now
+                    start.totient
+                    Instant.now.toEpochMilli() - initial.toEpochMilli()
+                }
+                totientImprov <- Future {
+                    val iniital = Instant.now
+                    start.totientImprov
+                    Instant.now.toEpochMilli - iniital.toEpochMilli
+                }
+            } yield println(s"regularTotient - ${regularTotient} ms totientImprov - ${totientImprov}")
+            
+        }
 
         
 
