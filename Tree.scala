3b1d3ecb9f8b6fc7ef96b89d1ccf6504bc2c6826
Edward Huang <edwardgunawan880@yahoo.com>
2020-07-28 21:24:53 -0400

    P 56

--- Tree.scala
+++ Tree.scala
@@ -56,6 +56,29 @@ package binarytree {
                     }
                 }
         }
+        
+        
+        implicit class TreeOps[T](tree: Tree[T]) {
+            /*
+            P56 (**) Symmetric binary trees.
+                Let us call a binary tree symmetric if you can draw a vertical line through the root node and then the right subtree is the mirror image of the left subtree. Add an isSymmetric method to the Tree class to check whether a given binary tree is symmetric. Hint: Write an isMirrorOf method first to check whether one tree is the mirror image of another. 
+                We are only interested in the structure, not in the contents of the nodes.
+                scala> Node('a', Node('b'), Node('c')).isSymmetric
+                res0: Boolean = true
+            */
+            def isSymmetric: Boolean = {
+                def isSymmetricR(root:Tree[T], root2: Tree[T]): Boolean = (root, root2) match {
+                    case (Node(_, l1, r1), Node(_, l2,r2)) => 
+                        isSymmetricR(l1,r2) && isSymmetricR(r1, l2)
+                    case (End, End) => true
+                    case _ => false
+                }
+
+                isSymmetricR(tree, tree)
+            }
+        }
+        
+
     }
 
 }
